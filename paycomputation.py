9e2f416d45f0488897104876799cffbe96fc4441
StAnnesSecurity <stannessecurity@proton.me>
2025-08-08 12:20:30 -0400

    Created function and improved output

--- paycomputation.py
+++ paycomputation.py
@@ -1,20 +1,32 @@
-sh = input("Enter Hours: ")
-sr = input("Enter Rate: ")
+def computepay(hours, rate) :
+    # print("In computepay", hours,rate)
+    if hours > 40:
+        # print("Overtime")
+        reg = rate * hours
+        otp = (hours - 40.0) * (rate * 0.5) #Overtime at 1.5x rate
+        # print(reg,otp)
+        pay = reg + otp
+    else: 
+        # print("Regular")
+        pay = hours * rate
 
+    # print("Returning",pay)
+    return pay
+
+sh = input("Enter Hours: ")
 try:
     fh = float(sh)
+except:
+    print("ERROR: please enter numeric input")
+    quit()
+
+sr = input("Enter Rate: ")
+try:
     fr = float(sr)
 except:
     print("ERROR: please enter numeric input")
     quit()
 
-if fh > 40:
-    # print("Overtime")
-    reg = fr * fh
-    otp = (fh - 40.0) * (fr * 0.5)
-    # print(reg,otp)
-    xp = reg + otp
-else: 
-    # print("Regular")
-    xp = fh * fr
+xp = computepay(fh,fr)
+
 print("Pay: $",xp)
